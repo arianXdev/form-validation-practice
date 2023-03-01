b1f81fc4e96d0f99e2b9313b439a078ff57f7330
Arian Hosseini <arianhosseini@outlook.com>
2023-03-01 21:21:59 +0330

    Installed Animate.css library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
 import App from "./App";
+
+import "./index.css";
+import "animate.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
